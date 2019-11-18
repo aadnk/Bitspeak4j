2f7cedb795ba99fbbcb615ee2b082c37f597164e
Kristian Stangeland <kr_stang@hotmail.com>
2019-11-18 23:40:29 +0100

    Add GPLv2

--- src/main/java/com/comphenix/bitspeak/BitspeakEncoder.java
+++ src/main/java/com/comphenix/bitspeak/BitspeakEncoder.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2019  Kristian S. Stangeland
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 package com.comphenix.bitspeak;
 
 import java.io.BufferedReader;
